6e652299246ad25ef9a28865be39c534bc08d510
Takeshi Ota <singer.scottie@gmail.com>
2021-03-09 04:42:28 +0900

    fizz buzztest記述完了

--- ruby1.rb
+++ ruby1.rb
@@ -1,18 +1,35 @@
 def fizz_buzz(n)
    if n % 15 == 0 
-      'fizz_buzz'
+      'Fizz Buzz'
    elsif n % 5 == 0
-       'buzz'
+       'Buzz'
    elsif n % 3 == 0
-       'fizz'
+      'Fizz'
    else
-      n.to_i
+      n.to_s
    end
 end
 
-puts "数値を入力してください"
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+   def test_fizz_buzz
+      assert_equal '1', fizz_buzz(1)
+      assert_equal '2', fizz_buzz(2)
+      assert_equal 'Fizz', fizz_buzz(3)
+      assert_equal '4', fizz_buzz(4)
+      assert_equal 'Buzz', fizz_buzz(5)
+      assert_equal 'Fizz', fizz_buzz(6)
+      assert_equal 'Fizz Buzz', fizz_buzz(15)
+   end
+end
 
 
-   n = gets.to_i
 
-   puts fizz_buzz(n)
+
+# puts "数値を入力してください"
+
+
+#    n = gets.to_i
+
+#    puts fizz_buzz(n)
