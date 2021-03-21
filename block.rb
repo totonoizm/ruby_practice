7bf038b49f335db90284a131d1882932fec00938
Takeshi Ota <singer.scottie@gmail.com>
2021-03-21 13:06:59 +0900

    [finish]4.6.4

--- block.rb
+++ block.rb
@@ -1,5 +1,20 @@
-numbers = [1,2,3,4]
-sum = numbers.inject(0){ |result, n| result + n }
+def charge(age)
+  case age
+  when 0..5
+    0
+  when 6..10
+    300
+  when 11..18
+    500
+  else
+    2000
+  end
+end
 
-puts sum
+puts charge(10)
+puts charge(3)
+puts charge(190)
+puts charge(10)
+
+
 
